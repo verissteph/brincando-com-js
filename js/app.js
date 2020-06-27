b24143f0b8ee416e76557389a06e588117a0dff0
verissteph <stephanieverissimopp@gmail.com>
2020-06-27 14:58:11 -0300

    input value was catched

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let botaoAdicionar = document.querySelector('#btn-adc');
+let input = document.querySelector('#task')
+
+
+botaoAdicionar.addEventListener('click', () => {
+
+    console.log('oi, você clicou em mim?');
+    //console.log(input.value);
+    let conteudoInput = input.value;
+
+
+})
